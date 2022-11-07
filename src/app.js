e9d1f3a37cc173ef7f7494896b9afd0d0a235df2
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-07 08:08:15 -0500

    [add] Any tests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { v4 } from "uuid";
 
 const app = express();
+app.use(express.json());
 
 app.get('/ping',(req, res) =>{
   res.send('pong');
@@ -9,5 +11,17 @@ app.get('/ping',(req, res) =>{
 app.get('/task',(req, res) =>{
   res.json([]);
 });
+
+app.post('/task', (req, res) => {
+  const {title, description} = req.body;
+
+  if(!title || !description) return res.sendStatus(400);
+
+  res.json({
+    title,
+    description,
+    id: v4()
+  });
+})
 
 export default app;
--- test/index.spec.js
+++ test/index.spec.js
@@ -13,3 +13,49 @@ describe('GET /task', () => {
   });
 
 });
+
+describe('POST /task', () => {
+
+  describe('Given a title and description', () => {
+
+    const newTask = {
+      title: 'New title',
+      description: 'Description demo'
+    }
+
+    // Should respond with a 200 status code
+    test('Should respond with a 200 status code',async() => {
+      const response = await request(app).post('/task').send(newTask);
+      expect(response.statusCode).toBe(200);
+    });
+
+    // Should respond with  a content-type of application/json
+    test('Should have a content-type: application/json in header',async() => {
+      const response = await request(app).post('/task').send(newTask);
+      expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+    });
+
+    // Should respond with a json object containing the new task with an id
+    test('Should respond with an Test ID', async() => {
+      const response = await request(app).post('/task').send(newTask);
+      expect(response.body.id).toBeDefined();
+    });
+  });
+
+  describe('When title and description missing', () => {
+
+    test('Should respond wit a 400 status code', async() => {
+      const fields = [
+        {},
+        {title: 'New title'},
+        {description: 'Description demo'}
+      ];
+      for(const body of fields){
+        const response = await request(app).post('/task').send(body);
+        expect(response.statusCode).toBe(400);
+      }
+    });
+    
+  });
+
+});
